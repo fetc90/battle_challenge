0ffaa998fcc8e72bea43d3584879361be6cad06a
Student <magnusarbuthnott@gmail.com>
2019-05-14 15:09:31 +0100

    Added <form> element to our view file. Added age and color param to the named-cat route.

--- app.rb
+++ app.rb
@@ -12,7 +12,15 @@ get '/name' do
   "Hi there Magnus"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  @age = params[:age]
+  @color = params[:color]
   erb(:index)
 end
